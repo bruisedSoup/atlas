aabfd94dfc73709806624e529feb5feb77c433ed
bruisedSoup <gonzales.isabella1414@gmail.com>
2026-08-16 15:07:21 +0800

    refactor(app): transfer element assets to src/elements, remove app/dashboard and app/thevault, and configure rewrites

--- atlas-app/next.config.ts
+++ atlas-app/next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  async rewrites() {
+    return [
+      {
+        source: "/dashboard",
+        destination: "/src/pages/dashboard",
+      },
+      {
+        source: "/thevault",
+        destination: "/src/pages/thevault",
+      },
+    ];
+  },
 };
 
 export default nextConfig;

48e771211d2b70eecba00e5f6a8394d4b47dd969
bruisedSoup <gonzales.isabella1414@gmail.com>
2026-08-16 11:52:14 +0800

    feat(api): add CustomLabelViewSet and custom_label filtering to TaskViewSet

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,7 +1,18 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from .models import Task
-from .serializers import TaskSerializer
+from .models import Task, CustomLabel
+from .serializers import TaskSerializer, CustomLabelSerializer
+
+
+class CustomLabelViewSet(viewsets.ModelViewSet):
+    serializer_class = CustomLabelSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return CustomLabel.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class TaskViewSet(viewsets.ModelViewSet):
@@ -16,6 +27,9 @@ class TaskViewSet(viewsets.ModelViewSet):
         label_param = self.request.query_params.get("label_type")
         if label_param:
             qs = qs.filter(label_type=label_param)
+        custom_label_param = self.request.query_params.get("custom_label")
+        if custom_label_param:
+            qs = qs.filter(custom_label=custom_label_param)
         course_id = self.request.query_params.get("course")
         if course_id:
             qs = qs.filter(course_id=course_id)

6aadd6206125069ef97dc949209db6d20c519ce9
bruisedSoup <gonzales.isabella1414@gmail.com>
2026-08-16 11:52:14 +0800

    feat(api): register custom-labels endpoint in tasks urls

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet
+from .views import TaskViewSet, CustomLabelViewSet
 
 router = DefaultRouter()
+router.register(r"custom-labels", CustomLabelViewSet, basename="custom-labels")
 router.register(r"", TaskViewSet, basename="tasks")
 
 urlpatterns = router.urls
